fa799baaf17dc4fe23fcce7bf43b93a7c533bfdc
MADANI <101865800+apolocine@users.noreply.github.com>
2024-04-18 02:15:44 +0200

    Update config.php

--- IP-Harvest-MultiUser/config.php
+++ IP-Harvest-MultiUser/config.php
@@ -1,15 +1,11 @@
 <?php
 // Connexion à la base de données (à personnaliser avec vos propres informations de connexion)
 
-$servername = "db5015489964.hosting-data.io";
-$db_username = "dbu719104";
-$db_password = "AWeefoo@26";
-$db_name = "dbs12656493";
-
-/**
+ 
+ 
 $servername = "localhost";
 $db_username = "htmc";
 $db_password = "gjetfm26";
 $db_name = "ips_db";
-*/
+ 
 ?>
