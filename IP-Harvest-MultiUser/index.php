33a7f6045f9b641e796b193d682f6e1b85115daa
MADANI <101865800+apolocine@users.noreply.github.com>
2024-04-18 02:07:54 +0200

    Update index.php

--- IP-Harvest-MultiUser/index.php
+++ IP-Harvest-MultiUser/index.php
@@ -2,7 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
- 
+ include_once 'config.php'; // Inclure le fichier config.php
+
 /**
 CREATE TABLE ips_users (
     id INT AUTO_INCREMENT PRIMARY KEY,
